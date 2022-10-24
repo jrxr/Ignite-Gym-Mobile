0c828479e35776d61b57894c43435033d46293ba
Agacy junior <juniormartinssom@gmail.com>
2022-10-24 20:05:01 -0300

    Application source

--- App.tsx
+++ App.tsx
@@ -1,8 +1,16 @@
+import { Text, View } from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <>
-      
-    </>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      {fontsLoaded ? <Text>Hello World</Text> : <View />}
+    </View>
   );
 }
-
