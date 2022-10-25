656e8cb6f038bf94744253a335211cdad13f91aa
Agacy junior <juniormartinssom@gmail.com>
2022-10-25 20:10:06 -0300

    Using SVG

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,16 +1,25 @@
-import { VStack, Image } from "native-base";
+import { VStack, Image, Text, Center } from "native-base";
 
-import BackgroundImg from '@assets/background.png';
+import LogoSvg from "@assets/logo.svg";
+import BackgroundImg from "@assets/background.png";
 
 export function SignIn() {
   return (
     <VStack flex={1} bg="gray.700">
-      <Image 
+      <Image
         source={BackgroundImg}
         alt="Pessoas treinando"
         resizeMode="contain"
         position="absolute"
       />
+
+      <Center my={24}>
+        <LogoSvg />
+
+        <Text color="gray.100" fontSize="sm">
+          Treine sua mente e o seu corpo.
+        </Text>
+      </Center>
     </VStack>
   );
 }
