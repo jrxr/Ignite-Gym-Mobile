2ef4402f4c2b9b37ca74f78d62a2ff7c5dc85cf7
Agacy junior <juniormartinssom@gmail.com>
2022-10-25 19:42:59 -0300

    Using NativeBase

--- App.tsx
+++ App.tsx
@@ -6,6 +6,8 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 
+import { Loading } from '@components/Loading';
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
@@ -17,7 +19,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <View /> : <View />}
+      {fontsLoaded ? <View /> : <Loading />}
     </NativeBaseProvider>
   );
 }
