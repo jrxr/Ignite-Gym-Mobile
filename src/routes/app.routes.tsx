001e37eda631fe811347d4481f68984c88db9f6e
Agacy junior <juniormartinssom@gmail.com>
2022-10-27 20:22:56 -0300

    Typing App Routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,34 +1,34 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {
+  createBottomTabNavigator,
+  BottomTabNavigationProp,
+} from "@react-navigation/bottom-tabs";
 
-import { Home } from '@screens/Home';
-import { Exercise } from '@screens/Exercise';
-import { History } from '@screens/History';
-import { Profile } from '@screens/Profile';
+import { Home } from "@screens/Home";
+import { Exercise } from "@screens/Exercise";
+import { History } from "@screens/History";
+import { Profile } from "@screens/Profile";
 
-const { Navigator, Screen } = createBottomTabNavigator();
+type AppRoutes = {
+  home: undefined;
+  exercise: undefined;
+  profile: undefined;
+  history: undefined;
+};
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes() {
   return (
     <Navigator>
-      <Screen 
-        name='home'
-        component={Home}
-      />
+      <Screen name="home" component={Home} />
 
-      <Screen 
-        name='history'
-        component={History}
-      />
+      <Screen name="history" component={History} />
 
-      <Screen 
-        name='profile'
-        component={Profile}
-      />
+      <Screen name="profile" component={Profile} />
 
-      <Screen 
-        name='exercise'
-        component={Exercise}
-      />
+      <Screen name="exercise" component={Exercise} />
     </Navigator>
   );
 }
