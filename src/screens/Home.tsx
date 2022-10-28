98caf7774ab91531117a434bf72a1487ddee1ecc
Agacy junior <juniormartinssom@gmail.com>
2022-10-28 20:02:35 -0300

    Exercise list header

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,5 @@
 import { HomeHeader } from "@components/HomeHeader";
-import { FlatList, HStack, VStack } from "native-base";
+import { FlatList, Heading, HStack, Text, VStack } from "native-base";
 import { Group } from "@components/Group";
 import { useState } from "react";
 
@@ -34,6 +34,18 @@ export function Home() {
         my={10}
         maxH={10}
       />
+
+      <VStack px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   );
 }
