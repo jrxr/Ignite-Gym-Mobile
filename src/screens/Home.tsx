63e6d17dde1318b29bd147cee2cd73f3984f07d2
Agacy junior <juniormartinssom@gmail.com>
2022-10-28 19:51:24 -0300

    Displaying the selected group

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,26 @@
 import { HomeHeader } from '@components/HomeHeader';
 import { HStack, VStack } from 'native-base';
 import { Group } from '@components/Group';
+import { useState } from 'react';
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('costa')
+
   return (
     <VStack flex={1}>
       <HomeHeader />
 
       <HStack>
-        <Group name="Costa" />
-        <Group name="ombro" />
+      <Group 
+          name="costa" 
+          isActive={groupSelected === 'costa'}
+          onPress={() => setGroupSelected('costa')}
+        />
+        <Group 
+          name="ombro" 
+          isActive={groupSelected === 'ombro'}
+          onPress={() => setGroupSelected('ombro')}
+        />
       </HStack>
     </VStack>
   );
