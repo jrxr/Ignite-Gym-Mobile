370bc0633953d19d209b52bc0ce6e99d92df5814
Agacy junior <juniormartinssom@gmail.com>
2022-10-28 08:57:21 -0300

    Positioning vertically and horizontally

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,17 @@
+import { Heading, HStack, Text, VStack } from 'native-base';
+
+export function HomeHeader() {
+  return (
+    <HStack>
+      <VStack>
+        <Text color="gray.100">
+          Olá,
+        </Text>
+
+        <Heading color="gray.100">
+          Júnior
+        </Heading>
+      </VStack>
+    </HStack>
+  );
+}
