b22a695df055713d5c430ecbde5603a38bf6fbca
Agacy junior <juniormartinssom@gmail.com>
2022-10-29 10:54:18 -0300

    HistoryCard Component

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,10 +1,16 @@
 import { ScreenHeader } from '@components/ScreenHeader';
+import { HistoryCard } from '@components/HistoryCard';
+
 import { VStack } from 'native-base';
 
 export function History() {
   return (
     <VStack flex={1}>
       <ScreenHeader title='Histórico' />
+
+      <HistoryCard />
+      <HistoryCard />
+      <HistoryCard />
     </VStack>
   );
 }
