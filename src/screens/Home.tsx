09b91a6e365ac0335b068952a6760bc25f85cb28
Agacy junior <juniormartinssom@gmail.com>
2022-10-29 10:49:44 -0300

    ScreenHeader Component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -31,7 +31,7 @@ export function Home() {
         renderItem={({ item }) => (
           <Group
             name={item}
-            isActive={groupSelected === item}
+            isActive={groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()}
             onPress={() => setGroupSelected(item)}
           />
         )}
