b7529f36677be36eae4849087a102e144c150532
Agacy junior <juniormartinssom@gmail.com>
2022-10-31 12:27:13 -0300

    Using toast messages

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 
-import { Alert, TouchableOpacity } from 'react-native';
+import { Alert, TouchableOpacity } from "react-native";
 import {
   Center,
   ScrollView,
@@ -9,7 +9,9 @@ import {
   Skeleton,
   Text,
   Heading,
+  useToast,
 } from "native-base";
+
 import * as ImagePicker from "expo-image-picker";
 import * as FileSystem from "expo-file-system";
 
@@ -23,6 +25,8 @@ const PHOTO_SIZE = 33;
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
   const [userPhoto, setUserPhoto] = useState("https://github.com/jrxr.png");
+
+  const toast = useToast();
 
   async function handleUserPhotoSelected() {
     setPhotoIsLoading(true);
@@ -41,9 +45,13 @@ export function Profile() {
 
       if (photoSelected.uri) {
         const photoInfo = await FileSystem.getInfoAsync(photoSelected.uri);
-        
-        if(photoInfo.size && (photoInfo.size  / 1024 / 1024 ) > 5){
-          return Alert.alert('Essa imagem é muito grande. Escolha uma de até 5MB.'); 
+
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
+          return toast.show({
+            title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
+            placement: 'top',
+            bgColor: 'red.500'
+          })
         }
 
         setUserPhoto(photoSelected.uri);
