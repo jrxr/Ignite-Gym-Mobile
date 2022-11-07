04fbfc7bb19f526ff315c7826d1b7fd93117a73f
Agacy junior <juniormartinssom@gmail.com>
2022-11-07 19:53:11 -0300

    inputs with states

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,24 +1,42 @@
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
 
-import LogoSvg from '@assets/logo.svg';
-import BackgroundImg from '@assets/background.png';
+import LogoSvg from "@assets/logo.svg";
+import BackgroundImg from "@assets/background.png";
 
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
 import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 
 export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
   const navigation = useNavigation();
 
   function handleGoBack() {
     navigation.goBack();
   }
 
+  function handleSignUp() {
+    console.log({
+      name,
+      email,
+      password,
+      passwordConfirm,
+    });
+  }
+
   return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
-        <VStack flex={1} px={10} pb={16}>
-        <Image 
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} px={10} pb={16}>
+        <Image
           source={BackgroundImg}
           defaultSource={BackgroundImg}
           alt="Pessoas treinando"
@@ -39,27 +57,33 @@ export function SignUp() {
             Crie sua conta
           </Heading>
 
-          <Input 
-            placeholder="Nome"
-          />
+          <Input placeholder="Nome" onChangeText={setName} />
 
-          <Input 
-            placeholder="E-mail" 
+          <Input
+            placeholder="E-mail"
             keyboardType="email-address"
             autoCapitalize="none"
-
+            onChangeText={setEmail}
           />
-          <Input 
-            placeholder="Senha" 
+
+          <Input
+            placeholder="Senha"
             secureTextEntry
+            onChangeText={setPassword}
           />
 
-          <Button title="Criar e acessar" />
+          <Input
+            placeholder="Confirmar a Senha"
+            secureTextEntry
+            onChangeText={setPasswordConfirm}
+          />
+
+          <Button title="Criar e acessar" onPress={handleSignUp} />
         </Center>
 
-        <Button 
-          title="Voltar para o login" 
-          variant="outline" 
+        <Button
+          title="Voltar para o login"
+          variant="outline"
           mt={24}
           onPress={handleGoBack}
         />
