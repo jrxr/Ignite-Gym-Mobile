1e8dcf928547af5aad40f4aa3c4245bfefdabd6b
Agacy junior <juniormartinssom@gmail.com>
2022-11-29 15:21:13 -0300

    Fetching user data from the backend

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,10 +1,11 @@
 import { createContext, ReactNode, useState } from "react";
 
 import { UserDTO } from "@dtos/UserDTO";
+import { api } from "@services/api";
 
 export type AuthContextDataProps = {
   user: UserDTO;
-  singIn: (email: string, password: string) => void;
+  singIn: (email: string, password: string) => Promise<void>;
 };
 
 type AuthContextProviderProps = {
@@ -16,20 +17,18 @@ export const AuthContext = createContext<AuthContextDataProps>(
 );
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
-  const [user, setUser] = useState({
-    id: "1",
-    name: "Junior",
-    email: "jrxr@email.com",
-    avatar: "jrxr.png",
-  });
+  const [user, setUser] = useState<UserDTO>({} as UserDTO)
 
-  function singIn(email: string, password: string) {
-    setUser({
-      id: "",
-      name: "",
-      email,
-      avatar: "",
-    });
+  async function singIn(email: string, password: string) {
+    try {
+      const { data } = await api.post('/sessions', { email, password });
+
+      if(data.user) {
+        setUser(data.user);
+      }
+    } catch (error) {
+      throw error
+    }
   }
 
   return (
