001cbaaca1774bb1dfa76e23413ddd0535ee5beb
Agacy junior <juniormartinssom@gmail.com>
2022-11-30 09:18:34 -0300

    Retrieving token from storage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -4,7 +4,10 @@ import {
   storageUserRemove,
   storageUserSave,
 } from "@storage/storageUser";
-import { storageAuthTokenSave } from "@storage/storageAuthToken";
+import {
+  storageAuthTokenSave,
+  storageAuthTokenGet,
+} from "@storage/storageAuthToken";
 
 import { UserDTO } from "@dtos/UserDTO";
 import { api } from "@services/api";
@@ -29,15 +32,18 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [isLoadingUserStorageData, setIsLoadingUserStorageData] =
     useState(true);
 
-  async function storageUserAndToken(userData: UserDTO, token: string) {
+  async function userAndTokenUpdate(userData: UserDTO, token: string) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+    setUser(userData);
+  }
+
+  async function storageUserAndTokenSave(userData: UserDTO, token: string) {
     try {
       setIsLoadingUserStorageData(true);
 
-      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-
       await storageUserSave(userData);
       await storageAuthTokenSave(token);
-      setUser(userData);
     } catch (error) {
       throw error;
     } finally {
@@ -50,10 +56,13 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       const { data } = await api.post("/sessions", { email, password });
 
       if (data.user && data.token) {
-        storageUserAndToken(data.user, data.token);
+        await storageUserAndTokenSave(data.user, data.token);
+        userAndTokenUpdate(data.user, data.token);
       }
     } catch (error) {
       throw error;
+    } finally {
+      setIsLoadingUserStorageData(false);
     }
   }
 
@@ -71,10 +80,13 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
   async function loadUserData() {
     try {
-      const userLogged = await storageUserGet();
+      setIsLoadingUserStorageData(true);
 
-      if (userLogged) {
-        setUser(userLogged);
+      const userLogged = await storageUserGet();
+      const token = await storageAuthTokenGet();
+
+      if (token && userLogged) {
+        userAndTokenUpdate(userLogged, token);
       }
     } catch (error) {
       throw error;
--- src/storage/storageAuthToken.ts
+++ src/storage/storageAuthToken.ts
@@ -1,7 +1,13 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { AUTH_STORAGE } from '@storage/storageConfig';
+import { AUTH_STORAGE } from "@storage/storageConfig";
 
 export async function storageAuthTokenSave(token: string) {
   await AsyncStorage.setItem(AUTH_STORAGE, token);
+}
+
+export async function storageAuthTokenGet() {
+  const token = await AsyncStorage.getItem(AUTH_STORAGE);
+
+  return token;
 }
