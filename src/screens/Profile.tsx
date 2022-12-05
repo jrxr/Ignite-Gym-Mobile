465cff2d598efc04911b5a183b751e17189659c7
Agacy junior <juniormartinssom@gmail.com>
2022-12-05 14:04:25 -0300

    Updating user data in storage and state

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -68,7 +68,7 @@ export function Profile() {
   const [userPhoto, setUserPhoto] = useState("https://github.com/jrxr.png");
 
   const toast = useToast();
-  const { user } = useAuth();
+  const { user, updateUserProfile } = useAuth();
   const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
     defaultValues: {
       name: user.name,
@@ -116,7 +116,13 @@ export function Profile() {
     console.log(data);
     try {
       setIsUpdating(true);
+
+      const userUpdated = user;
+      userUpdated.name = data.name;
+
       await api.put('/users', data);
+
+      await updateUserProfile(userUpdated);
 
       toast.show({
         title: 'Perfil atualizado com sucesso!',
