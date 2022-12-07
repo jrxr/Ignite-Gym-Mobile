f0545f91c32496f32e8514cde78285807c4e8986
Agacy junior <juniormartinssom@gmail.com>
2022-12-07 09:09:58 -0300

    Queuing requests

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,21 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosError, AxiosInstance } from "axios";
 import { storageAuthTokenGet } from "@storage/storageAuthToken";
 
 import { AppError } from "@utils/AppError";
 
 type SignOut = () => void;
 
+type PromiseType = {
+  resolve: (value?: unknown) => void;
+  reject: (reason: unknown) => void;
+};
+
 type APIInstanceProps = AxiosInstance & {
   registerInterceptTokenManager: (signOut: SignOut) => () => void;
 };
+
+let isRefreshing = false;
+let failedQueue: Array<PromiseType> = [];
 
 const api = axios.create({
   baseURL: "http://localhost:3333",
@@ -28,6 +36,23 @@ api.registerInterceptTokenManager = (singOut) => {
             singOut();
             return Promise.reject(requestError);
           }
+
+          const originalRequest = requestError.config;
+
+          if (isRefreshing) {
+            return new Promise((resolve, reject) => {
+              failedQueue.push({ resolve, reject });
+            })
+              .then((token) => {
+                originalRequest.headers.Authorization = `Bearer ${token}`;
+                return axios(originalRequest);
+              })
+              .catch((error) => {
+                throw error;
+              });
+          }
+
+          isRefreshing = true;
         }
 
         singOut();
